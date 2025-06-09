be0d48b49ae1ff98b12a174254ddf01eca8c82fc
Rahelaraya <rahelaraya26@gmail.com>
2025-06-09 11:41:54 +0200

    Initial commit of solution files (excluding .vs)

---
+++ API/Controllers/ServiceController.cs
@@ -0,0 +1,57 @@
+﻿using Application.DTO;
+using Application.Interface;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceController : ControllerBase
+                
+    {
+        
+        private readonly IServiceRepository _IServiceRepository;
+
+        public ServiceController(IServiceRepository serviceRepository) {
+
+                        _IServiceRepository = serviceRepository;
+        }
+
+        // GET: api/<ServiceController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ServiceDto>>> GetAll()
+       {
+           var Services = await _IServiceRepository.GetAllAsync();
+           return Ok(Services);
+        }
+     
+
+        // GET api/<ServiceController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ServiceController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ServiceController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ServiceController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interface;
+using Domain.Entities;
+using Infrastructure.DataBase;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+                
+            });
+            
+            services.AddScoped<IServiceRepository, ServiceRepository>();
+            return services;
+
+        }
+    }
+}
